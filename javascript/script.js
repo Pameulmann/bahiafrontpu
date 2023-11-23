0bea5aba7e488c942cb69e4e47c4226e5b63185a
Pameulmann <145028989+Pameulmann@users.noreply.github.com>
2023-11-23 00:39:50 -0300

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+const toggle = document.getElementById("toggle");
+const body = document.body;
+
+toggle.addEventListener("click", function() {
+    body.classList.toggle("modo-oscuro");
+});
+
+function cartel()  {alert("Datos: Pamela Ulmann\FrontEnd Developer\Email: pameu23@gmail.com")}
